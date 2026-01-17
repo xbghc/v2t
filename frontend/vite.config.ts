0a4995318ac8d2e48daf069a1bf8e57ffa48fd08
xbghc <ghm20191606@gmail.com>
2026-01-17 21:31:17 +0800

    fix: 添加vite base配置

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
     plugins: [vue(), tailwindcss()],
+    base: '/v2t/',
     resolve: {
         alias: {
             '@': resolve(__dirname, 'src')
